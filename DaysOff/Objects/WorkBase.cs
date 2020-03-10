bf61152a526aae392db42e285ee84376f5889edc
Mungert69 <43328293+Mungert69@users.noreply.github.com>
2020-03-10 10:45:13 +0000

    Added New work types

--- DaysOff/Objects/WorkBase.cs
+++ DaysOff/Objects/WorkBase.cs
@@ -5,7 +5,7 @@ namespace DaysOff.Objects
 {
     public enum WorkTypes
     {
-        OHC=0, HC=1, MC=2, KA=3, DB=4, GR=5, OGR=6, IT=7, BY=8, PR=9, OJ=10, SH=11
+        OHC=0, HC=1, MC=2, KA=3, DB=4, GR=5, OGR=6, IT=7, BY=8, PR=9, OJ=10, SH=11, SR=12, LC=13,PL=14, RAP=15
     }
 
    
@@ -28,6 +28,10 @@ namespace DaysOff.Objects
             WorkNames.Add("Promotions");
             WorkNames.Add("Own jobs");
             WorkNames.Add("Shopping");
+            WorkNames.Add("Sewing Repair");
+            WorkNames.Add("Linen Cuboard");
+            WorkNames.Add("Plant Care");
+            WorkNames.Add("RAP Management");
 
         }
 
